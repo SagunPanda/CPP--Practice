a0b1f86e193ffed90c14d123c60fdafa603e21cf
SagunPanda <pandasagun@gmail.com>
2022-05-29 09:26:40 +0530

    basic qus

---
+++ multiplication_table.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++){
+        {
+            cout<<"24 *" << i << "="<<24 * i<<endl;
+        }
+    }
+    for(i=1;i<=10;i++){
+    {
+        cout<<"50 *" << i << "="<<24 * i<<endl;
+    }
+    }
+    
+    return 0;
+}
